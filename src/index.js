cd556383bb5d2cc0817ac21b18bfe2897cb5c768
Praison Benjamin <praisonbenjamin@gmail.com>
2020-11-24 21:13:42 -0600

    final submit

--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@ import 'normalize.css';
 import './index.css';
 
 import shoppingList from './shopping-list';
+import api from './api';
+import store from './store';
 
 const main = function () {
+  api.getItems()
+  .then((items) => {
+    items.forEach((item) => store.addItem(item));
+    shoppingList.render();
+  });
+
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
